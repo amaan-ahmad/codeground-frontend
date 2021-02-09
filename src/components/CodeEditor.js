f276258a53899acf6b81ba45c2131b2cdf3d9bb3
Amaan Ahmad <amaanahmad582001@gmail.com>
2021-02-09 16:03:04 +0530

    template added + styled 🎨

--- src/components/CodeEditor.js
+++ src/components/CodeEditor.js
@@ -1,19 +1,35 @@
-import { UnControlled as CodeMirror } from "react-codemirror2";
-import { useState } from "react";
+import { Controlled as CodeMirror } from "react-codemirror2";
+import { useState, useEffect } from "react";
 import axios from "axios";
+import { cppTemplate, javaTemplate } from "./data";
 require("codemirror/mode/xml/xml");
 require("codemirror/mode/javascript/javascript");
 
 export default function CodeEditor() {
   const [editorValue, setEditorValue] = useState("");
   const [output, setOutput] = useState("");
+  const [stdin, setStdin] = useState("");
   const [lang, setLang] = useState("cpp");
+  const [isEdited, setIsEdited] = useState(false);
+
+  useEffect(() => {
+    if (isEdited) {
+      return;
+    }
+    if (lang === "cpp") {
+      setEditorValue(cppTemplate);
+    } else if (lang === "java") {
+      setEditorValue(javaTemplate);
+    }
+  }, [lang]);
+
   const handleRun = () => {
     axios
       .post("https://codeground-backend.herokuapp.com/api/exec", {
         language: lang,
         versionIndex: "3",
         script: editorValue,
+        stdin: stdin,
       })
       .then((res) => {
         console.log(res.data);
@@ -28,24 +44,51 @@ export default function CodeEditor() {
   };
   return (
     <>
-      <select name="languages" onChange={handleLangChange} value={lang}>
-        <option value="cpp">C++</option>
-        <option value="java">Java</option>
-      </select>
-      <CodeMirror
-        value={editorValue}
-        options={{
-          mode: "text/x-c++src",
-          theme: "material",
-          lineNumbers: true,
-        }}
-        className="editor"
-        onChange={(editor, data, value) => {
-          setEditorValue(value);
-        }}
-      />
-      <button onClick={handleRun}>Run</button>
-      <textarea value={output} readOnly={true} />
+      <div className="editor-panel">
+        <select name="languages" onChange={handleLangChange} value={lang}>
+          <option value="cpp">C++</option>
+          <option value="java">Java</option>
+        </select>
+        <CodeMirror
+          value={editorValue}
+          options={{
+            mode: "text/x-c++src",
+            theme: "cobalt",
+            lineNumbers: true,
+          }}
+          className="editor"
+          onBeforeChange={(editor, data, value) => {
+            if (!isEdited) {
+              console.log("edited.");
+              setIsEdited(true);
+            }
+            setEditorValue(value);
+          }}
+          onChange={(editor, data, value) => {
+            setEditorValue(value);
+          }}
+        />
+        <div className="flex flex-row">
+          <CodeMirror
+            value={stdin}
+            options={{
+              mode: "text",
+              theme: "material",
+            }}
+            className="stdin"
+            onBeforeChange={(editor, data, value) => {
+              setStdin(value);
+            }}
+            onChange={(editor, data, value) => {
+              setStdin(value);
+            }}
+          />
+          <textarea value={output} readOnly={true} className="output-panel" />
+        </div>
+        <button className="btn-run" onClick={handleRun}>
+          Run
+        </button>
+      </div>
     </>
   );
 }
---
+++ src/components/data.js
@@ -0,0 +1,19 @@
+export const cppTemplate = `#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "follow @amaancodes on Twitter :)";
+    
+    return 0;
+}
+`;
+
+export const javaTemplate = `public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		System.out.println("follow @amaancodes on Twitter :)");
+	}
+}`;
